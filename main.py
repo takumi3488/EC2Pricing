4bd33c65c9a9f9fd0c8dc66f68a9569c846e4cf3
takumi <51111242+takumi3488@users.noreply.github.com>
2022-03-16 12:58:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import csv
+
+def parse(file: str):
+  with open(f'{file.lower()}.html') as f:
+    s = BeautifulSoup(f.read(), 'html.parser')
+  res = []
+  for tr in s.find_all('tr', class_='awsui-table-row'):
+    blocks = list(map(lambda x: x.text.strip(), tr.select('td>span>span')))
+    title = blocks[0]
+    price = float(f"{(float(blocks[1].split()[0])*120*24*30.5):.1f}")
+    vcpu = int(blocks[2])
+    mem = float(blocks[3].split(' ')[0])
+    res.append({'title': title, 'price': price, 'vcpu': vcpu, 'mem': mem})
+  res = list(sorted(res, key=lambda x: x['price']))
+  with open(f'{file}.csv', 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=["title", "price", "vcpu", "mem"])
+    writer.writeheader()
+    writer.writerows(res)
+
+parse("Osaka")
+parse("Tokyo")
